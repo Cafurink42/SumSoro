512df1750cb935242a3e8ed13f707d7cd07560dd
Daniel <carlosdanielguth@gmail.com>
2024-02-26 22:32:30 -0300

    Adicionando botão de relatórios e iconizando botão sair

--- src/index.php
+++ src/index.php
@@ -1,5 +1,4 @@
 <?php
-// Inicialize a sessão
 session_start();
 
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
@@ -25,13 +24,20 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <link rel="icon" href="SumSoroIcons/sumsoroicon.jpg">
     <link rel="manifest" href="manifest.json">
     <style>
-        #buttonLogout{
+        #buttonLogout {
             border: none;
             background-color: transparent;
             color: red;
         }
-    </style>
 
+        .gerar_relatorio {
+            border: none;
+            background-color: transparent;
+        }
+        .button_exit{
+c            color:black;
+        }
+    </style>
     <title>SumSoro</title>
 </head>
 
@@ -39,18 +45,14 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-expand-lg">
         <a class="navbar-brand" href="#">Navbar</a>
 
-        <a class="navbar-toggler" href = "logout.php" id  = "buttonLogout">
-             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-             </svg>
-         </a>
-   
-  
-
+        <a class="navbar-toggler" href="logout.php" id="buttonLogout">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
+        </a>
         </div>
     </nav>
-
     <div class="Initial-Front">
         <h1 class="title">SumSoro</h1>
         <br>
@@ -59,26 +61,33 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         <input type="number" placeholder="Digite o volume de soro" class="input" min=0 title="Digite a quantidade de soro desejada" id="input_area">
         <br>
         <br>
+
         <button id="button-done" class="custom-buttonDone" alt="botão salva valor">
             <span class="material-symbols-outlined" id="button_done">check</span>
         </button>
+
         <button id="button-doneAll" class="custom-buttonDoneAll" title="Salvar volume">
             <span class="material-symbols-outlined" id="button_done_all">done_all</span>
         </button>
 
-        <a href="logout.php">logout</a>
+        <button id="button-relate" title="Gerar relatório" class="gerar_relatorio">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+                <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+            </svg>
 
+        </button>
+        <a href="logout.php" class  = "button_exit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+            </svg>
+        </a>
     </div>
     <div class="output-qtd" id="qtd">
-
-
     </div>
 
-    <!--  <footer class = "footer">
-      <img class  = "icon-sumsoro" src = "SumSoroIcons/sumsoroicon.jpg"  title  ="SumSoroIcone"> 
-        <p>Versão 1.0</p>
-    </footer>
-  -->
+
     <script src="./script.js" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
